963d19ee8f1cbc34fc6f216d6b8eed7dcaf5c8d8
natnael_esk <natnaeleskinder41@gmail.com>
2024-04-07 20:53:52 +0300

    go

--- src/Projects.js
+++ src/Projects.js
@@ -1,24 +1,52 @@
-// import Gymate from "./images/gymate-home.webp";
-// import Raouf from "./images/ecom.webp";
+import ProBox from "./ProBox";
+import geeez from "./images/geez.png";
+import CoinDom from "./images/coindom-full.png";
+
 // import ProBox from "./ProBox";
-// import CarRental from "./images/car-rental-full.webp";
-// import CoinDom from "./images/coindom-full.png";
 
 
 const Projects = () => {
     return ( 
-        <>
-        <section id="projects" className="project">
-          <div className="container">
-            {/* <div className="project-content">
-              <p>portfolio</p>
-              <h3>Each project is a unique piece of development 🧩</h3>
-              
-            </div> */}
-            <h2 className="new-projects">New projects coming soon.. 🤠</h2>
+      <>
+      <section id="projects" className="project">
+        <div className="container">
+          <div className="project-content">
+            <p>portfolio</p>
+            <h3>Each project is a unique piece of development 🧩</h3>
+            <div className="projects-grid">
+              <ProBox
+                title="Geez Shoes"
+                img={geeez}
+                date="(Apr 2024)"
+                description="A dedicated platform for local businesses to showcase their premium leather footwear and expand their reach to a wider audience online."
+                techno1="React"
+                techno2="Tailwind"
+                code="https://github.com/natnaelesk/Geez"
+                demo="https://natnaelesk.github.io/Geez/"
+                scrollY="-83%"
+                icon="👞"
+              />
+
+              <ProBox
+                title="Dorm Delivery"
+                date="(March 2024)"
+                img={CoinDom}
+                description="Mobile-friendly website for local cafe dorm delivery. Order easily from your phone!"
+                techno1="Django"
+                techno2="Tailwind"
+                code="https://github.com/natnaelesk/WaseDormDelivery"
+                demo="http://natnaelesk.pythonanywhere.com/"
+                scrollY="-74%"
+                icon="🚚"
+                cName="reversed-proj"
+              />
+
+            </div>
           </div>
-        </section>
-      </>
+          {/* <h2 className="new-projects">New projects coming soon.. 🤠</h2> */}
+        </div>
+      </section>
+    </>
      );
 }
  
